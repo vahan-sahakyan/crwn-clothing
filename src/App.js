b650a81135acf84002b7797df6230069f2cf98fb
Vahan Sahakyan <vahan.sahakyan@bpcarm.org>
2022-03-10 18:22:38 +0400

    Initial Commit 🚀

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import HomePage from './pages/homepage/homepage.component.jsx';
+
+const App = () => (
+  <h1>
+    <HomePage />
+  </h1>
+);
 
 export default App;
