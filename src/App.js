5f9577a6a1b24c2fefc3970d14cc1f600105cd74
Vahan Sahakyan <vahan.sahakyan@bpcarm.org>
2022-04-26 21:14:08 +0400

    finished sign-in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
-          console.log(this.state);
         });
       }
       this.setState({ currentUser: userAuth });
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils.js';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -15,8 +15,15 @@ class SignIn extends React.Component {
       password: '',
     };
   }
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (err) {
+      console.log(err);
+    }
 
     this.setState({ email: '', password: '' });
   };
