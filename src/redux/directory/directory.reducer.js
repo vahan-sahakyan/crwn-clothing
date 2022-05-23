6067edc25a4915cc9b430831c9e501f751e979fd
Vahan Sahakyan <vahan.sahakyan@bpcarm.org>
2022-05-23 21:19:24 +0400

    added directory images

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,38 +2,33 @@ const INITIAL_STATE = {
   sections: [
     {
       title: 'womens',
-      imageUrl:
-        'https://images.pexels.com/photos/4862517/pexels-photo-4862517.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-4862517.jpg&fm=jpg',
+      imageUrl: '/src/assets/img/directory/womens.jpg',
       size: 'large',
       id: 2,
       linkUrl: 'shop/womens',
     },
     {
       title: 'mens',
-      imageUrl:
-        'https://images.pexels.com/photos/842811/pexels-photo-842811.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-842811.jpg&fm=jpg',
+      imageUrl: '../../assets/img/directory/mens.jpg',
       size: 'large',
       id: 1,
       linkUrl: 'shop/mens',
     },
     {
       title: 'hats',
-      imageUrl:
-        'https://images.pexels.com/photos/1751150/pexels-photo-1751150.jpeg?cs=srgb&dl=pexels-rodolfo-quirós-1751150.jpg&fm=jpg',
+      imageUrl: '/src/assets/img/directory/hats.jpg',
       id: 3,
       linkUrl: 'shop/hats',
     },
     {
       title: 'jackets',
-      imageUrl:
-        'https://images.pexels.com/photos/7679798/pexels-photo-7679798.jpeg?cs=srgb&dl=pexels-mart-production-7679798.jpg&fm=jpg',
+      imageUrl: '/src/assets/img/directory/jackets.jpg',
       id: 4,
       linkUrl: 'shop/jackets',
     },
     {
       title: 'sneakers',
-      imageUrl:
-        'https://images.pexels.com/photos/6777991/pexels-photo-6777991.jpeg?cs=srgb&dl=pexels-hipkicks-6777991.jpg&fm=jpg',
+      imageUrl: '/src/assets/img/directory/sneakers.jpg',
       id: 5,
       linkUrl: 'shop/sneakers',
     },
